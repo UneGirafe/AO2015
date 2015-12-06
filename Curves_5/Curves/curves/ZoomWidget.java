e3ee3c8c2adca6dd301fe0c5ad3743c37defa6f5
UneGirafe <gautier.couvrat@gmail.com>
2015-12-06 15:23:31 +0100

    ZOB

--- Curves_5/Curves/curves/ZoomWidget.java
+++ Curves_5/Curves/curves/ZoomWidget.java
@@ -10,7 +10,6 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.JTextField;
-import javax.swing.JTextPane;
 
 public class ZoomWidget extends Observable {
 
@@ -26,21 +25,20 @@ public class ZoomWidget extends Observable {
 
 	public enum Position { LEFT ("1"), CENTER("2") , RIGHT("3") ;
 		private String pos;
-		
+
 		private Position(String x){
 			this.pos = x;
 		}
-		
+
 		public String toString(){
 			return pos;
 		}
 	}
-	
+
 	public ZoomWidget() {
 		widget = new JPanel();
 		widget.setLayout(new GridLayout(3,3));
-		
-		
+
 		xMin = new JTextField();
 		yMin = new JTextField();
 		xMax = new JTextField();
@@ -50,17 +48,15 @@ public class ZoomWidget extends Observable {
 		maxLabel = new JLabel("max");
 		yLabel = new JLabel("y");
 		xLabel = new JLabel("x");
-		
-		widget.add(minLabel,Position.CENTER);
-		widget.add(maxLabel,Position.RIGHT);
-		widget.add(xLabel,Position.LEFT);
-		widget.add(xMin, Position.CENTER);
-		widget.add(xMax, Position.RIGHT);
+
+		widget.add(minLabel);
+		widget.add(maxLabel);
+		widget.add(xLabel);
+		widget.add(xMin);
+		widget.add(xMax);
 		
 	}
-	
 
-	
 	public JPanel getWidget() {
 		return widget;
 	}
